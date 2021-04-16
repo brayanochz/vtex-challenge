65aa69b000c381de257e6ab50c87935e277221f3
Brayan Camargo <bcmarago@unicesar.edu.co>
2021-04-16 12:13:54 -0500

    Initial commit

---
+++ vtexchallengeback/app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var { graphqlHTTP } = require('express-graphql');
+
+var indexRouter = require('./routes/index');
+const { schema } = require('./graphql/schema');
+const { root } = require('./graphql/root');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.status(404).send('No se ha encontado')
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send(err);
+});
+
+module.exports = app;
